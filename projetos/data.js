7ab008335b8a684b104028e5ea1bbe7d7c974088
aglairdev <github.panama228@passfwd.com>
2026-06-13 03:01:18 -0400

    chore: atualiza descrições dos projetos

--- projetos/data.js
+++ projetos/data.js
@@ -1,20 +1,20 @@
 window.PROJECTS = [
 {
     title: '~/goflix',
-    description: 'Acervo de vídeos no terminal.',
+    description: 'Gerenciador de vídeos mpv no terminal, histórico de assistidos e suporte a multi-idiomas.',
     tags: ['Go', 'TUI', 'Linux', 'MPV'],
     github: 'https://github.com/aglairdev/goflix',
   },  
 {
     title: '~/brasa',
-    description: 'Kanban e diário.',
+    description: 'Kanban, diário criptografado, sons ambiente e suporte a multi-idiomas.',
     tags: ['JavaScript', 'Web Crypto API', 'CSP'],
     github: 'https://github.com/aglairdev/brasa',
     site: 'https://aglairdev.github.io/brasa/'
   },
 {
     title: '~/manifest',
-    description: 'Buscador de manifests.',
+    description: 'Buscador de manifests no terminal que integra ProtonDB e Accela.',
     tags: ['Bash', 'TUI', 'Linux'],
     github: 'https://github.com/aglairdev/Manifest',
   },
