4464e372d1553e8e1873be4014035bb34978881b
aglairdev <github.panama228@passfwd.com>
2026-07-02 13:45:24 -0400

    chore: adiciona projetos/steam-cli

--- projetos/data.js
+++ projetos/data.js
@@ -1,21 +1,27 @@
 window.PROJECTS = [
-{
+  {
     title: '~/goflix',
     description: 'Gerenciador de vídeos mpv no terminal, histórico de assistidos e suporte a multi-idiomas.',
     tags: ['Go', 'TUI', 'Linux', 'MPV'],
     github: 'https://github.com/aglairdev/goflix',
-  },  
-{
-    title: '~/brasa',
-    description: 'Kanban, diário criptografado, sons ambiente e suporte a multi-idiomas.',
-    tags: ['JavaScript', 'Web Crypto API', 'CSP'],
-    github: 'https://github.com/aglairdev/brasa',
-    site: 'https://aglairdev.github.io/brasa/'
   },
-{
+  {
     title: '~/manifest',
     description: 'Buscador de manifests no terminal que integra ProtonDB e Accela.',
     tags: ['Bash', 'TUI', 'Linux'],
     github: 'https://github.com/aglairdev/Manifest',
   },
+  {
+    title: '~/steam-cli',
+    description: 'Biblioteca Steam no terminal, com lançamento nativo e via Proton.',
+    tags: ['Bash', 'TUI', 'Linux'],
+    github: 'https://github.com/aglairdev/steam-cli',
+  },
+  {
+    title: '~/brasa',
+    description: 'Kanban, diário criptografado, sons ambiente e suporte a multi-idiomas.',
+    tags: ['JavaScript', 'Web Crypto API', 'CSP'],
+    github: 'https://github.com/aglairdev/brasa',
+    site: 'https://aglairdev.github.io/brasa/'
+  }
 ];
